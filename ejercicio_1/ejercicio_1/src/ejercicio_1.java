1b70dfcbb3c5bc3f26598e0362ce90ca894e1783
Danielfmv <44736460+Danielfmv@users.noreply.github.com>
2022-10-15 15:40:58 -0500

    Add files via upload

---
+++ ejercicio_1/ejercicio_1/src/ejercicio_1.java
@@ -0,0 +1,27 @@
+public class ejercicio_1 {
+
+    public static void main(String[] args) {
+        // int, long, double, boolean, String.
+        int edad = 18;
+        long edadTierra = 4543;
+        double edadJuan = 22.45;
+        boolean conoceJava = true;
+        String holaMundo = "Hola mundo, estoy programando en Java";
+        byte maxbyte = 127;
+        long largo = 1000000L;
+        short minshort = -32768;
+        float varpi = 3.1416f;
+        char caracter = 'h';
+
+        System.out.println(edad);
+        System.out.println(edadTierra);
+        System.out.println(edadJuan);
+        System.out.println(conoceJava);
+        System.out.println(holaMundo);
+        System.out.println(maxbyte);
+        System.out.println(largo);
+        System.out.println(minshort);
+        System.out.println(varpi);
+        System.out.println(caracter);
+    }
+}
