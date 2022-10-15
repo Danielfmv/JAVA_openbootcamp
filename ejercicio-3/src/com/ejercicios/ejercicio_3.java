9eacefe93459b7990bc0b44c652de6eeda5598c5
Danielfmv <44736460+Danielfmv@users.noreply.github.com>
2022-10-15 18:31:54 -0500

    Add files via upload

---
+++ ejercicio-3/src/com/ejercicios/ejercicio_3.java
@@ -0,0 +1,16 @@
+package com.ejercicios;
+
+import java.util.Arrays;
+
+public class ejercicio_3 {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Pepito", "Juanito", "Andrew", "Sultanate"};
+
+        for (String nombre: nombres) {
+            System.out.println(nombre);
+        }
+    }
+}
+
